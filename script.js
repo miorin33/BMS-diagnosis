2f610ae4d52c74149f1d68715774306807f3fd28
miorin33 <miorin.maria.go@gmail.com>
2025-03-13 12:25:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ document.getElementById("finish").addEventListener("click", function() {
     <h2>BMS 体、心、魂の健康診断</h2>
     <p style="font-size:18px;">体：${results.body}　心：${results.mind}　魂：${results.soul}</p>
     <a href="https://o09guhda.autosns.app/cp/JLvSX32Zaq?uid=[[uid]]" target="_blank" style="display:inline-block; margin-top:20px;">
-      <img src="https://imgur.com/a/GSjWpvB" alt="診断結果詳細" style="max-width:100%; height:auto;">
+      <img src="https://f.yourl.jp/5a23aca7/" alt="診断結果詳細" style="max-width:100%; height:auto;">
     </a>
   `;
 });
